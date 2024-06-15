c67b20c270965b223538febc7312e83819ea5c9f
Kapil Chaudhary <kc685838@gmail.com>
2024-06-16 01:29:39 +0530

    Updated: Tested Button & CardOpinionProduct component

--- pages/test.vue
+++ pages/test.vue
@@ -2,9 +2,8 @@
     <div class="p-4">
         <div class="my-8">
             <CardOpinionProduct />
-        </div>
-        <div class="my-8">
-            <CardOpinionProduct />
+
+            <Button to="/" label="Click me!" />
         </div>
     </div>
 </template>
