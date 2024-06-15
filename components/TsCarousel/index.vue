99dbf5c194e1a87e21e450b9a07b5cca9793689e
Kapil Chaudhary <kc685838@gmail.com>
2024-06-16 01:42:39 +0530

    Added: TsCarousel component

---
+++ components/TsCarousel/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <Carousel :items-to-show="1">
+        <Slide v-for="slide in 10" :key="slide">
+            {{ slide }}
+        </Slide>
+
+        <template #addons>
+            <Navigation />
+            <Pagination />
+        </template>
+    </Carousel>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
