26cef3dc830d14c1d827c76b85aa6058e8c33eaf
Kapil Chaudhary <kc685838@gmail.com>
2024-06-16 01:17:56 +0530

    Added: Typescript in Button

--- components/Button/index.vue
+++ components/Button/index.vue
@@ -1,12 +1,33 @@
 <template>
-    <button class="text-4xl">{{ props.text }}</button>
+  <component
+    :is="tag_element"
+    :type="props.type"
+    :to="props.to || props.href"
+    :class="$attrs.class || twMerge('text-4xl')"
+  >
+    <slot>
+      {{ props.label }}
+    </slot>
+  </component>
 </template>
 
-<script setup>
-const props = defineProps({
-    text: {
-        type: String,
-        default: 'Button'
-    },
-})
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  to?: string;
+  href?: string;
+  label?: string;
+  
+  type?: "button" | "submit" | "reset";
+};
+
+const props = defineProps<Props>();
+
+const tag_element = computed(() => {
+  if (props.to || props.href) return resolveComponent("NuxtLink");
+
+  return "button";
+});
+
 </script>
