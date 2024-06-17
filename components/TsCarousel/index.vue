7a6ec32859cf6b5632dbac7e3f067dd304db2447
Kapil Chaudhary <kc685838@gmail.com>
2024-06-17 09:14:05 +0530

    Updated: Props and Styling of Carousel

--- components/TsCarousel/index.vue
+++ components/TsCarousel/index.vue
@@ -1,55 +1,251 @@
 <template>
-    <Carousel :items-to-show="1" :wrap-around="props.wrapAround">
-        <Slide v-for="slide in 2" :key="slide">
-            {{ slide }}
-        </Slide>
+  <Carousel v-model="model" :items-to-show="1" :wrap-around="props.wrapAround">
+    <slot>
+      <Slide v-for="slide in 2" :key="slide">
+        {{ slide }}
+      </Slide>
+    </slot>
 
-        <template #addons="{ slidesCount }">
-            <Navigation v-if="slidesCount > 1" />
-            <Pagination v-if="slidesCount > 1" />
-        </template>
-    </Carousel>
+    <template #addons="{ slidesCount }">
+      <Navigation v-if="slidesCount > 1" />
+      <Pagination v-if="slidesCount > 1" />
+    </template>
+  </Carousel>
 </template>
 
 <script setup lang="ts">
-
-// modelValue & i18n still pending
-
-type SnapAlign = {
-    snapAlign?: "start" | "end" | "center" | "center-odd" | "center-even";
+// i18n types
+type I18n = {
+  ariaNextSlide?: string;
+  ariaPreviousSlide?: string;
+  ariaNavigateToSlide?: string;
+  ariaGallery?: string;
+  itemXofY?: string;
+  iconArrowUp?: string;
+  iconArrowDown?: string;
+  iconArrowRight?: string;
+  iconArrowLeft?: string;
 };
 
-type Props = SnapAlign & {
-    itemsToShow?: number;
-    itemsToScroll?: number;
-    wrapAround?: boolean;
-    transition?: number;
-    autoplay?: number;
-    breakpoints?: object | null;
-    mouseDrag?: boolean;
-    touchDrag?: boolean;
-    pauseAutoplayOnHover?: boolean;
-    dir?: "ltr" | "rtl";
-    i18n?: object | null;
-}
+// Props types
+type Props = {
+  itemsToShow?: number;
+  itemsToScroll?: number;
+  wrapAround?: boolean;
+  snapAlign?: "start" | "end" | "center" | "center-odd" | "center-even";
+  transition?: number;
+  autoplay?: number;
+  breakpoints?: object | null;
+  mouseDrag?: boolean;
+  touchDrag?: boolean;
+  pauseAutoplayOnHover?: boolean;
+  dir?: "ltr" | "rtl";
+  i18n?: I18n | null;
+};
 
+// props with default values
 const props = withDefaults(defineProps<Props>(), {
-    itemsToShow: 1,
-    itemsToScroll: 1,
-    wrapAround: false,
-    snapAlign: "center",
-    transition: 300,
-    autoplay: 0,
-    breakpoints: null,
-    mouseDrag: true,
-    touchDrag: true,
-    pauseAutoplayOnHover: false,
-    dir: "ltr"
+  itemsToShow: 1,
+  itemsToScroll: 1,
+  wrapAround: false,
+  snapAlign: "center",
+  transition: 300,
+  autoplay: 0,
+  breakpoints: null,
+  mouseDrag: true,
+  touchDrag: true,
+  pauseAutoplayOnHover: false,
+  dir: "ltr",
+  i18n: () => ({
+    ariaNextSlide: "Navigate to next slide",
+    ariaPreviousSlide: "Navigate to previous slide",
+    ariaNavigateToSlide: "Navigate to slide {slideNumber}",
+    ariaGallery: "Gallery",
+    itemXofY: "Item {currentSlide} of {slidesCount}",
+    iconArrowUp: "Arrow pointing upwards",
+    iconArrowDown: "Arrow pointing downwards",
+    iconArrowRight: "Arrow pointing to the right",
+    iconArrowLeft: "Arrow pointing to the left",
+  }),
 });
 
+// modelValue
+const model = defineModel<number>();
+
+// slot props = {slideWidth, currentSlide, slidesCount}
 </script>
 
-<style scoped lang="scss">
-
+<style>
+:root {
+    /* Color */
+    --vc-clr-primary: red;
+    --vc-clr-secondary: #090f207f;
+    --vc-clr-white: #ffffff;
+  
+    /* Icon */
+    --vc-icn-width: 1.2em;
+  
+    /* Navigation */
+    --vc-nav-width: 30px;
+    --vc-nav-height: 30px;
+    --vc-nav-border-radius: 0;
+    --vc-nav-color: var(--vc-clr-primary);
+    --vc-nav-color-hover: var(--vc-clr-secondary);
+    --vc-nav-background: transparent;
+  
+    /* Pagination */
+    --vc-pgn-width: 12px;
+    --vc-pgn-height: 4px;
+    --vc-pgn-margin: 4px;
+    --vc-pgn-border-radius: 0;
+    --vc-pgn-background-color: var(--vc-clr-secondary);
+    --vc-pgn-active-color: var(--vc-clr-primary);
+  }
+  
+  .carousel {
+    position: relative;
+    text-align: center;
+    box-sizing: border-box;
+    touch-action: pan-y;
+    overscroll-behavior: none;
+  }
+  
+  .carousel.is-dragging {
+    touch-action: none;
+  }
+  
+  .carousel * {
+    box-sizing: border-box;
+  }
+  
+  .carousel__track {
+    display: flex;
+    padding: 0 !important;
+    position: relative;
+  }
+  
+  .carousel__viewport {
+    overflow: hidden;
+  }
+  
+  .carousel__sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
+  
+  .carousel__icon {
+    width: var(--vc-icn-width);
+    height: var(--vc-icn-width);
+    fill: currentColor;
+  }
+  
+  .carousel__prev,
+  .carousel__next {
+    box-sizing: content-box;
+    background: var(--vc-nav-background);
+    border-radius: var(--vc-nav-border-radius);
+    width: var(--vc-nav-width);
+    height: var(--vc-nav-height);
+    text-align: center;
+    font-size: var(--vc-nav-height);
+    padding: 0;
+    color: var(--vc-nav-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    border: 0;
+    cursor: pointer;
+    margin: 0 10px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  
+  .carousel__next--disabled,
+  .carousel__prev--disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+  
+  .carousel__prev {
+    left: 0;
+  }
+  
+  .carousel__next {
+    right: 0;
+  }
+  
+  .carousel--rtl .carousel__prev {
+    left: auto;
+    right: 0;
+  }
+  
+  .carousel--rtl .carousel__next {
+    right: auto;
+    left: 0;
+  }
+  
+  @media (hover: hover) {
+    .carousel__prev:hover,
+    .carousel__next:hover {
+      color: var(--vc-nav-color-hover);
+    }
+  }
+  
+  .carousel__pagination {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    line-height: 0;
+    margin: 10px 0 0;
+    padding: 0;
+  }
+  
+  .carousel__pagination-button {
+    display: block;
+    border: 0;
+    margin: 0;
+    cursor: pointer;
+    padding: var(--vc-pgn-margin);
+    background: transparent;
+  }
+  
+  .carousel__pagination-button::after {
+    display: block;
+    content: '';
+    width: var(--vc-pgn-width);
+    height: var(--vc-pgn-height);
+    border-radius: var(--vc-pgn-border-radius);
+    background-color: var(--vc-pgn-background-color);
+  }
+  
+  .carousel__pagination-button--active::after {
+    background-color: var(--vc-pgn-active-color);
+  }
+  
+  @media(hover: hover) {
+    .carousel__pagination-button:hover::after {
+      background-color: var(--vc-pgn-active-color);
+    }
+  }
+  
+  .carousel__slide {
+    scroll-snap-stop: auto;
+    flex-shrink: 0;
+    margin: 0;
+    position: relative;
+  
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  
+    /* Fix iOS scrolling #22 */
+    transform: translateZ(0);
+  }
 </style>
-
--- pages/test.vue
+++ pages/test.vue
@@ -5,7 +5,24 @@
 
             <Button to="/" label="Click me!" /> -->
 
-            <TsCarousel />
+            <TsCarousel>
+                <Slide key="1">
+                    <Card
+                        title="This is the title"
+                        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ea soluta ratione quis quia eveniet optio sequi et culpa, expedita in nesciunt ullam similique, numquam temporibus atque maiores nam accusantium laborum molestiae magni illo, iusto inventore? Voluptatum modi quibusdam aliquam!"
+                        footer="This is the footer"
+                        root-class="border rounded-3xl"
+                    />
+                </Slide>
+                <Slide key="2">
+                    <Card
+                        title="This is the title"
+                        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ea soluta ratione quis quia eveniet optio sequi et culpa, expedita in nesciunt ullam similique, numquam temporibus atque maiores nam accusantium laborum molestiae magni illo, iusto inventore? Voluptatum modi quibusdam aliquam!"
+                        footer="This is the footer"
+                        root-class="border rounded-3xl"
+                    />
+                </Slide>
+            </TsCarousel>
         </div>
     </div>
 </template>
