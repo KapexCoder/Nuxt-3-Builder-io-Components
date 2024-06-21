219f2dfd8cb1880027e5ba2deb78e33ab41440a3
Kapil Chaudhary <kc685838@gmail.com>
2024-06-21 22:19:30 +0530

    Added: TsCarouselTabs component

---
+++ components/TsCarousel/Tabs/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <TsCarousel :breakpoints="breakpoints" :pagination="false" ref="tabCarousel">
+    <Slide v-for="tab in 10" :key="tab">
+      <Button
+        v-if="tab % 2 === 0"
+        append-class="text-nowrap"
+        :label="`Button --> ${tab}`"
+      />
+      <Button
+        v-else
+        append-class="text-nowrap"
+        :label="`Button mast h --> ${tab}`"
+      />
+    </Slide>
+  </TsCarousel>
+</template>
+
+<script setup lang="ts">
+const tabCarousel = ref(null);
+
+console.log("tabCarousel ----> ", tabCarousel);
+
+const breakpoints = {
+  // 0px and up
+  0: {
+    itemsToShow: 5,
+    itemsToScroll: 3.5,
+    snapAlign: "center",
+  },
+  // 1024 and up
+  1024: {
+    itemsToShow: 5,
+    itemsToScroll: 5,
+    snapAlign: "start",
+  },
+};
+</script>
