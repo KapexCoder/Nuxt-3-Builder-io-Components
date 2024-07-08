0d6e2722c8ca523036a98f9b67fe9618c48f2094
Kapil Chaudhary <kc685838@gmail.com>
2024-07-08 22:14:26 +0530

    Updated: TsCarouselTabs

--- pages/test.vue
+++ pages/test.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="p-4">
-        <div class="my-8">
-            <!-- <CardOpinionProduct />
+  <div class="p-4">
+    <div class="my-8">
+      <!-- <CardOpinionProduct /> -->
 
-            <Button to="/" label="Click me!" /> -->
+      <!-- <Button to="/" label="Click me!" /> -->
 
-            <TsCarousel>
-                <Slide key="1">
-                    <Card
-                        title="This is the title"
-                        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ea soluta ratione quis quia eveniet optio sequi et culpa, expedita in nesciunt ullam similique, numquam temporibus atque maiores nam accusantium laborum molestiae magni illo, iusto inventore? Voluptatum modi quibusdam aliquam!"
-                        footer="This is the footer"
-                        root-class="border rounded-3xl"
-                    />
-                </Slide>
-                <Slide key="2">
-                    <Card
-                        title="This is the title"
-                        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ea soluta ratione quis quia eveniet optio sequi et culpa, expedita in nesciunt ullam similique, numquam temporibus atque maiores nam accusantium laborum molestiae magni illo, iusto inventore? Voluptatum modi quibusdam aliquam!"
-                        footer="This is the footer"
-                        root-class="border rounded-3xl"
-                    />
-                </Slide>
-            </TsCarousel>
-        </div>
+      <!-- <TsCarousel>
+        <Slide key="1">
+          <Card
+            title="This is the title"
+            body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ea soluta ratione quis quia eveniet optio sequi et culpa, expedita in nesciunt ullam similique, numquam temporibus atque maiores nam accusantium laborum molestiae magni illo, iusto inventore? Voluptatum modi quibusdam aliquam!"
+            footer="This is the footer"
+            root-class="border rounded-3xl"
+          />
+        </Slide>
+        <Slide key="2">
+          <Card
+            title="This is the title"
+            body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ea soluta ratione quis quia eveniet optio sequi et culpa, expedita in nesciunt ullam similique, numquam temporibus atque maiores nam accusantium laborum molestiae magni illo, iusto inventore? Voluptatum modi quibusdam aliquam!"
+            footer="This is the footer"
+            root-class="border rounded-3xl"
+          />
+        </Slide>
+      </TsCarousel> -->
+
+      <TsCarouselTabs />
     </div>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
